94ea90b895f9fe0e0c474f6ce560fe731cad5e91
Parjanya Modi <parjanya.cs19@bmsce.ac.in>
2021-12-30 16:04:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 const express = require("express");
-const https = require("https");
 const bodyparser = require("body-parser");
 const cors = require("cors");
-const fs = require("fs");
 
 const app = express();
 const port = 3073;
 
-var n=0;
+var n = 0;
+app.use(express.json());
 
-app.get('/:namespace/:keyname', (req, res) => {
-    var nameSpace = req.params.namespace;
-    var keyName = req.params.keyname;
-    /*fs.readFile("./main.json", "utf8", (err, data) => {
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get("/:namespace/:keyname", (req, res) => {
+  var nameSpace = req.params.namespace;
+  var keyName = req.params.keyname;
+  /*fs.readFile("./main.json", "utf8", (err, data) => {
         res.end(data);
     });*/
-    res.end(`${n}`);
-    n++;
+  res.end(`{name:"${nameSpace}",key:"${keyName}",visits:"${n}"}`);
+  n++;
 });
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.listen(port, () => {
-    console.log(`App listenting at ${port}`)
+  console.log(`App listenting at ${port}`);
 });
